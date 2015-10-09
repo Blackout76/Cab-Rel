7d285ab31458f8e7d73d8893f2644d4157a13401
Blackout76 <olivier.leger@imerir.com>
2015-10-09 23:10:07 +0200

    PC Client, FIX constructor

--- Clients/PC/src/Map/Mapbridge.java
+++ Clients/PC/src/Map/Mapbridge.java
@@ -1,17 +1,15 @@
 package Map;
 
-import org.json.simple.JSONObject;
-
 public class Mapbridge {
 	private String startVertice;
 	private String endArea;
 	private String endVertice;
 	private float weight;
 
-	public Mapbridge(String startVertice2,String endArea2, String endVertice2,float weight) {
+	public Mapbridge(String startVertice2,String endArea2, String endVertice,float weight) {
 		this.startVertice = startVertice2;
 		this.endArea = endArea2;
-		this.endVertice = this.endVertice;
+		this.endVertice = endVertice;
 		this.weight = weight;
 	}
 }
