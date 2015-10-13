161232a5129711b031eab559e4f9d87c7ccca5c9
Blackout76 <olivier.leger@imerir.com>
2015-10-13 20:22:19 +0200

    Client PC, addind getter about the start point of any bridge

--- Clients/PC/src/Map/MapBridge.java
+++ Clients/PC/src/Map/MapBridge.java
@@ -1,15 +1,19 @@
 package Map;
 
-public class Mapbridge {
+public class MapBridge {
 	private String startVertice;
 	private String endArea;
 	private String endVertice;
 	private float weight;
 
-	public Mapbridge(String startVertice2,String endArea2, String endVertice,float weight) {
+	public MapBridge(String startVertice2,String endArea2, String endVertice,float weight) {
 		this.startVertice = startVertice2;
 		this.endArea = endArea2;
 		this.endVertice = endVertice;
 		this.weight = weight;
+	}
+	
+	public String getStartVertice(){
+		return this.startVertice;
 	}
 }
