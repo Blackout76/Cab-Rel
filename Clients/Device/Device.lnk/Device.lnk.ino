e532b73171b0dbb0187b72f3dd652e8fa2fbf056
nicolasbzh <nicolas.casol@gmail.com>
2015-10-14 14:50:30 +0200

    init LCD and Cab's state

--- Clients/Device/Device.lnk/Device.lnk.ino
+++ Clients/Device/Device.lnk/Device.lnk.ino
@@ -1,33 +1,84 @@
+#include "Arduino.h"
 #include <Ethernet.h>
 #include <SPI.h>
+#include <LiquidCrystal.h>
 
+
+EthernetClient clientHttp;
+
+//Init of LCd and KeyPad
+// select the pins used on the LCD panel
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+// define some values used by the panel and buttons
+int lcd_key = 0;
+int adc_key_in = 0;
+#define btnRIGHT 0
+#define btnUP 1
+#define btnDOWN 2
+#define btnLEFT 3
+#define btnSELECT 4
+#define btnNONE 5
+
+// Cab's State
+bool stateFreeCab = true;
+
+// adress mac of the shield
 byte mac[] =   { 0x98, 0x4F, 0xEE, 0x05, 0x35, 0x20 }; 
-
+// the arduino's Ip Adresse  
 IPAddress ip(192, 168, 1, 225);
-EthernetClient client;
-String getHttp= "";
-char tmp;
-bool adr = false;
-String adrServ;
-String prtServ;
+// the dns server ip
+IPAddress dnServer(192, 168, 1, 1);
+// the router's gateway address:
+IPAddress gateway(192, 168, 1, 1);
+// the subnet:
+IPAddress subnet(255, 255, 255, 0);
+
+
+
+
+// read the buttons
+int read_LCD_buttons()
+{
+  adc_key_in = analogRead(0); // read the value from the sensor
+ //Serial.print(adc_key_in);
+  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+  // we add approx 50 to those values and check to see if we are close
+  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the
+  if (adc_key_in < 50) return btnRIGHT;
+  if (adc_key_in < 195) return btnUP;
+  if (adc_key_in < 380) return btnDOWN;
+  if (adc_key_in < 555) return btnLEFT;
+  if (adc_key_in < 790) return btnSELECT;
+  return btnNONE; // when all others fail, return this...
+}
 
 void setup()
 {  
+  String getHttp= "";
+  char tmp;
+  bool adr = false;
+  String adrServ;
+  String prtServ;
+
+  lcd.begin(16, 2); // start the library
+  
   Serial.begin(9600);
-  Ethernet.begin(mac, ip); // si dhcp activé
-  Serial.print("Galileo IP address: ");
+  Ethernet.begin(mac, ip, dnServer, gateway, subnet);
+  //print out the IP address
+  Serial.print("IP = ");
   Serial.println(Ethernet.localIP());
   
-  int erreur = client.connect("192.168.1.50", 800);
-  if (client.connected()) 
+  int erreur = clientHttp.connect("192.168.1.50", 800);
+  if (clientHttp.connected()) 
   {
-    client.println("GET /deviceConnect HTTP/1.0");
-    client.println();
+    clientHttp.println("GET /deviceConnect HTTP/1.0");
+    clientHttp.println();
     int cpt = 1;
+    int i=0;
      bool cond = false;
-    while (client.available()) 
+    while (clientHttp.available()) 
     {
-      char c = client.read();
+      char c = clientHttp.read();
       if(adr)
       {
         if(c == ':')
@@ -45,27 +96,65 @@ void setup()
             adrServ+=c;
           }
         }
-        //getHttp+=c;
       }
       if(c == '/' and tmp=='/') adr=true;
       Serial.print(c);
       tmp=c;
     }
-    //Serial.print(getHttp);
-    //adrServ = getHttp.substring(0,12);
-    // prtServ = getHttp.substring(13);
-     Serial.println("adr: "+adrServ);
-     Serial.print("prt: "+prtServ);
+    //disconnect to Http
+    clientHttp.stop();
+
+    Serial.println("adr :"+adrServ);
+    Serial.print("prt: "+prtServ);
+    
 
   } 
-  else {
+  else 
+  {
     Serial.println("connection failed");
   }
   
 }
 void loop () {
-   /* if(client.available()){
-    char c = client.read();
-    Serial.print(c);}*/
+
+   manageCabState();
+
+  lcd.setCursor(0,1); // move to the begining of the second line
+  //Manage the button
+  lcd_key = read_LCD_buttons(); // read the buttons
+  switch (lcd_key) // depending on which button was pushed, we perform an action
+  {
+    case btnRIGHT:
+    {
+      lcd.print("Refused   ");
+      break;
+    }
+    case btnLEFT:
+    {
+      lcd.print("Accept ");
+      break;
+    }
+    case btnUP:
+    {
+      lcd.print("UP      ");
+      break;
+    }
+    case btnDOWN:
+    {
+      lcd.print("DOWN ");
+      break;
+    }
+  
   }
+}
+
+void manageCabState()
+{
+   //display the cab's state
+  lcd.setCursor(0,0); // move to the begining of the first line
+  if(stateFreeCab) lcd.print("FREE");
+  else lcd.print("BUSY");
+}
+
+  
 
