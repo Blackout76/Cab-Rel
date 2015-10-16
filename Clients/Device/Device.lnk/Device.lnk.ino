39bc092fa68f673d6f2092fb7ffb13b90446d08d
nicolasbzh <nicolas.casol@gmail.com>
2015-10-16 14:56:42 +0200

    add manage of keypad shield.

--- Clients/Device/Device.lnk/Device.lnk.ino
+++ Clients/Device/Device.lnk/Device.lnk.ino
@@ -9,11 +9,22 @@ byte mac[] =  { 0x98, 0x4F, 0xEE, 0x05, 0x35, 0x20 };
 IPAddress ip(192, 168, 1, 225);
 
 
+// define some values used by the panel and buttons
+int lcd_key = 0;
+int adc_key_in = 0;
+#define btnRIGHT 0
+#define btnUP 1
+#define btnDOWN 2
+#define btnLEFT 3
+#define btnSELECT 4
+#define btnNONE 5
+
+
 void setup()
 {
   Serial.begin(9600);
-  Ethernet.begin(mac,ip);
-  
+  Ethernet.begin(mac, ip);
+
   //print out the IP address
   Serial.print("IP = ");
   Serial.println(Ethernet.localIP());
@@ -23,6 +34,54 @@ void setup()
 
 void loop()
 {
-
+  //Manage the button
+  lcd_key = read_LCD_buttons(); // read the buttons
+  switch (lcd_key) // depending on which button was pushed, we perform an action
+  {
+    case btnRIGHT:
+      {
+        break;
+      }
+    case btnLEFT:
+      {
+        break;
+      }
+    case btnUP:
+      {
+        break;
+      }
+    case btnDOWN:
+      {
+        break;
+      }
+    case btnSELECT:
+      {
+        break;
+      }
+    case btnNONE:
+      {
+        break;
+      }
+  }
 }
+
+
+
+// read the buttons
+int read_LCD_buttons()
+{
+  adc_key_in = analogRead(0); // read the value from the sensor
+  //Serial.print(adc_key_in);
+  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+  // we add approx 50 to those values and check to see if we are close
+  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the
+  if (adc_key_in < 50) return btnRIGHT;
+  if (adc_key_in < 195) return btnUP;
+  if (adc_key_in < 380) return btnDOWN;
+  if (adc_key_in < 555) return btnLEFT;
+  if (adc_key_in < 790) return btnSELECT;
+  return btnNONE; // when all others fail, return this...
+}
+
+
 
