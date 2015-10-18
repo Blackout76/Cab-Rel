2d9eb156f5a78b3aaad10f3d9b12de12472b2e48
Blackout76 <olivier.leger@imerir.com>
2015-10-18 19:39:50 +0200

    Client PC: Manage Taxi request

--- Clients/PC/src/Taxi/TaxiManager.java
+++ Clients/PC/src/Taxi/TaxiManager.java
@@ -1,7 +1,62 @@
 package Taxi;
 
+import java.awt.Point;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import General.Logger;
+import General.Logger.Logger_type;
+import General.Main;
+import General.CPoint;
+import General.Utils;
+import Map.MapStreet;
+import Render.RenderArea;
+
 public class TaxiManager {
 
-	
-	// TODO
+	public void createRequestAtPoint(Point startPoint){
+        TaxiRequest taxiRequest = new TaxiRequest(computeBestInterceptStreet(startPoint));
+       	Logger.log(Logger_type.SUCCESS, "[tAXI]", "new taxi request created !");
+       	System.out.println(taxiRequest.toJSON());
+       	Main.networkManager.sendTaxiRequest(taxiRequest.toJSON());
+	}
+
+	public HashMap<String, Object> computeBestInterceptStreet(Point ori){
+		HashMap<String, Object> result = new HashMap<>();
+		CPoint impactPoint = new CPoint();
+		double distance = -1;
+		HashMap<String, Object> streetInfos = null;
+		MapStreet street = null;
+        HashMap<String, MapStreet> streets = Main.mapManager.getAreaByName(Main.renderer.getNameOfActiveArea()).getStreets();
+        float areaX = Main.mapManager.getAreaByName(Main.renderer.getNameOfActiveArea()).getWidth();
+        float areaY = Main.mapManager.getAreaByName(Main.renderer.getNameOfActiveArea()).getHeight();
+        float ori_x = ori.x * areaX/RenderArea.width;
+        float ori_y = ori.y * areaY/RenderArea.height;
+
+        for(Entry<String, MapStreet> streetEntry : streets.entrySet()) {
+        	
+			HashMap<String, Object> infos = Utils.computeInfoOfTriangle(
+												streetEntry.getValue().getPath().get(0).toPoint() ,
+												streetEntry.getValue().getPath().get(1).toPoint() ,
+												new CPoint(ori_x, ori_y));
+	    	if(distance == -1){
+				streetInfos = (HashMap<String, Object>) infos.clone();
+				street = streetEntry.getValue();
+				distance = (double) streetInfos.get("height");
+			}
+			else
+				if(distance >= ((double)infos.get("height"))){
+					streetInfos = (HashMap<String, Object>) infos.clone();
+					street = streetEntry.getValue();
+					distance = (double) streetInfos.get("height");
+				}
+				else if(((HashMap<String, Double>)infos.get("pointIntercept")).get("distanceToPoint") != null && distance >= ((HashMap<String, Double>)infos.get("pointIntercept")).get("distanceToPoint")){
+					streetInfos = (HashMap<String, Object>) infos.clone();
+					street = streetEntry.getValue();
+					distance = (double) ((HashMap<String, Double>)infos.get("pointIntercept")).get("distanceToPoint");
+				}
+	    }
+        streetInfos.put("streetName", street.getName());
+		return streetInfos;
+	}
 }
--- Clients/PC/src/Taxi/TaxiRequest.java
+++ Clients/PC/src/Taxi/TaxiRequest.java
@@ -1,15 +1,42 @@
 package Taxi;
 
-import java.awt.Point;
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+
+import General.CPoint;
+import General.Main;
+import Map.MapArea;
+import Map.MapStreet;
+import Map.MapVertice;
+import Render.IHM;
 
 public class TaxiRequest {
-	private Point requestPoint;
+	private CPoint intersectedPoint;
+	private double pourcentIntersect;
+	private MapVertice originVertice;
+	private MapStreet street;
+	private MapArea area;
 	
-	public TaxiRequest (Point requestPoint){
-		//this.requestPoint;
+	public TaxiRequest (HashMap<String, Object> infos){
+		this.area = Main.mapManager.getAreaByName(IHM.getNameOfActiveArea());
+		this.street = this.area.getStreetByName((String) infos.get("streetName"));
+		this.originVertice = this.street.getPath().get( (int)( ((HashMap<String, Object>)infos.get("pointIntercept")).get("indexVertice") ) );
+		this.pourcentIntersect = (double)infos.get("pourcentHeight");
+		this.intersectedPoint = new CPoint( Float.parseFloat( ((HashMap<String, Object>)infos.get("pointIntercept")).get("x").toString() ),  
+				 							Float.parseFloat(((HashMap<String, Object>)infos.get("pointIntercept")).get("y").toString()	));
 	}
 	
-	public Point getRequestPoint(){
-		return this.requestPoint;
+	public JSONObject toJSON(){
+		JSONObject json = new JSONObject();
+		JSONObject infos = new JSONObject();
+		infos.put("origin", this.intersectedPoint.toJSON());
+		infos.put("area", this.area.getName());
+		infos.put("street", this.street.getName());
+		infos.put("pourcent", this.pourcentIntersect);
+		infos.put("vertice", this.originVertice.getName());
+		json.put("cabRequest", infos);
+		return json;
+		
 	}
 }
