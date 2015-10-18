bd4043b793548858169a033597dcc74f0a7e3c14
Blackout76 <olivier.leger@imerir.com>
2015-10-18 19:42:42 +0200

    Client PC: FIX tools function for computeTaxiRequest

--- Clients/PC/src/General/Utils.java
+++ Clients/PC/src/General/Utils.java
@@ -1,13 +1,11 @@
 package General;
 
-import java.awt.Point;
 import java.util.HashMap;
 
-import org.json.simple.JSONObject;
 
 public class Utils {
 
-	public static double computeAngleOrient(Point A, Point B, Point C){
+	public static double computeAngleOrient(CPoint A, CPoint B, CPoint C){
 		/*
 		 * Compute oriented angle between vector(AB) and vector (AC)
 		 */
@@ -24,7 +22,7 @@ public class Utils {
 		return angle_orient;
 	}
 
-	public static double computeHeightOfTriangle(Point A, Point B, Point C){
+	public static double computeHeightOfTriangle(CPoint A, CPoint B, CPoint C){
 		/*
 		 * Compute height of Triangle
 		 */
@@ -33,7 +31,7 @@ public class Utils {
 		double height = Math.sin(angle*(Math.PI/180))*norme_AC;
 		return height;
 	}
-	public static double computeDistOfHeightTriangle(Point A, Point B, Point C){
+	public static double computeDistOfHeightTriangle(CPoint A, CPoint B, CPoint C){
 		/*
 		 * Compute distance between A and the height of Triangle
 		 */
@@ -43,31 +41,52 @@ public class Utils {
 		
 		return dist;
 	}
-	public static HashMap<String, Object> computeInfoOfTriangle(Point A, Point B, Point C){
+	public static HashMap<String, Object> computeInfoOfTriangle(CPoint A, CPoint B, CPoint C){
 		HashMap<String, Object> result = new HashMap<>();
 		double norme_AB = Math.sqrt(Math.pow(B.getX() - A.getX(), 2)+Math.pow(B.getY() - A.getY(), 2));
 		result.put("height", computeHeightOfTriangle(A,B,C));
 		result.put("distHeight", computeDistOfHeightTriangle(A,B,C));
-		result.put("pourcentHeight", ((double)result.get("distHeight")*100) / (norme_AB) );
-		result.put("pointIntercept", computePointOfHeightIntercept((Double) result.get("distHeight"),A,B));
+		result.put("pourcentHeight", ((double)result.get("distHeight")) / (norme_AB) );
+		result.put("pointIntercept", computePointOfHeightIntercept((Double) result.get("distHeight"),A,B,C));
+		
+		/* Inverse origin point on street if %>50% */
+		if((double)result.get("pourcentHeight") > 0.5){
+			result.put("pourcentHeight", 1-((double)result.get("pourcentHeight")) );
+			if( (int)((HashMap<String, Object>)result.get("pointIntercept")).get("indexVertice") == 1)
+				((HashMap<String, Object>)result.get("pointIntercept")).put("indexVertice", 0);
+			else
+				((HashMap<String, Object>)result.get("pointIntercept")).put("indexVertice", 1);
+				
+		}
+			
 		return result;
 	}
 
-	private static HashMap<String, Double> computePointOfHeightIntercept(Double distHeight,Point A, Point B) {
-		HashMap<String, Double> result = new HashMap<>();
-		double angle_absis = computeAngleOrient(A,B,new Point( (int)(A.getX()+50) , (int) A.getY() ));
+	private static HashMap<String, Object> computePointOfHeightIntercept(Double distHeight,CPoint A, CPoint B, CPoint C) {
+		HashMap<String, Object> result = new HashMap<>();
+		double angle_absis = computeAngleOrient(A,B,new CPoint( (int)(A.getX()+50) , (int) A.getY() ));
 		double norme_AB = Math.sqrt(Math.pow(B.getX() - A.getX(), 2)+Math.pow(B.getY() - A.getY(), 2));
 		if(distHeight <= 0){
-			result.put("x", A.getX());
-			result.put("y", A.getY());
+			result.put("x", (double) A.getX());
+			result.put("y", (double) A.getY());
+			double norme_AC = Math.sqrt(Math.pow(C.getX() - A.getX(), 2)+Math.pow(C.getY() - A.getY(), 2));
+			result.put("distanceToPoint", norme_AC);
+			result.put("indexVertice",  0);
 		}
 		else if(distHeight >= norme_AB){
-			result.put("x", B.getX());
-			result.put("y", B.getY());
+			result.put("x", (double) B.getX());
+			result.put("y", (double) B.getY());
+			double norme_BC = Math.sqrt(Math.pow(C.getX() - B.getX(), 2)+Math.pow(C.getY() - B.getY(), 2));
+			result.put("distanceToPoint", norme_BC);
+			result.put("indexVertice",  1);
 		}
 		else{
 			result.put("x" , A.getX() + Math.cos(angle_absis*(Math.PI/180))*distHeight);
-			result.put("y" , A.getY() + Math.cos(angle_absis*(Math.PI/180))*distHeight);
+			if(A.getY() >= B.getY()) 
+				result.put("y" , A.getY() - Math.sin(angle_absis*(Math.PI/180))*distHeight);
+			else
+				result.put("y" , A.getY() + Math.sin(angle_absis*(Math.PI/180))*distHeight);
+			result.put("indexVertice",  0);
 		}
 		return result;
 	}
