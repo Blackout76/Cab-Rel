5086681c0c2d867e9114b4109dc34a5b1cdb672e
Blackout76 <olivier.leger@imerir.com>
2015-10-18 12:59:15 +0200

    Client PC: Added function to return information about Triangle, height, distHeight, point intercept of height, %distHeight

--- Clients/PC/src/General/Utils.java
+++ Clients/PC/src/General/Utils.java
@@ -1,6 +1,9 @@
 package General;
 
 import java.awt.Point;
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
 
 public class Utils {
 
@@ -21,7 +24,7 @@ public class Utils {
 		
 		return angle_orient;
 	}
-	
+
 	public static double computeHeightOfTriangle(Point A, Point B, Point C){
 		/*
 		 * Compute height of Triangle
@@ -30,6 +33,33 @@ public class Utils {
 		double norme_AC = Math.sqrt(Math.pow(C.getX() - A.getX(), 2)+Math.pow(C.getY() - A.getY(), 2));
 		double height = Math.sin(angle*(Math.PI/180))*norme_AC;
 		return height;
+	}
+	public static double computeDistOfHeightTriangle(Point A, Point B, Point C){
+		/*
+		 * Compute distance between A and the height of Triangle
+		 */
+		double angle = computeAngleOrient(A,B,C);
+		double norme_AC = Math.sqrt(Math.pow(C.getX() - A.getX(), 2)+Math.pow(C.getY() - A.getY(), 2));
+		double dist = Math.cos(angle*(Math.PI/180))*norme_AC;
+		
+		return dist;
+	}
+	public static HashMap<String, Object> computeInfoOfTriangle(Point A, Point B, Point C){
+		HashMap<String, Object> result = new HashMap<>();
+		double norme_AB = Math.sqrt(Math.pow(B.getX() - A.getX(), 2)+Math.pow(B.getY() - A.getY(), 2));
+		result.put("height", computeHeightOfTriangle(A,B,C));
+		result.put("distHeight", computeDistOfHeightTriangle(A,B,C));
+		result.put("pourcentHeight", ((double)result.get("distHeight")*100) / (norme_AB) );
+		result.put("pointIntercept", computePointOfHeightIntercept((Double) result.get("distHeight"),A,B));
+		return result;
+	}
+
+	private static HashMap<String, Double> computePointOfHeightIntercept(Double distHeight,Point A, Point B) {
+		HashMap<String, Double> result = new HashMap<>();
+		double angle_absis = computeAngleOrient(A,B,new Point( (int)(A.getX()+50) , (int) A.getY() ));
+		result.put("x" , A.getX() + Math.cos(angle_absis*(Math.PI/180))*distHeight);
+		result.put("y" , A.getY() + Math.cos(angle_absis*(Math.PI/180))*distHeight);
+		return result;
 	}
 	
 }
