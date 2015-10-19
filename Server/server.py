641ea3059eb17b83081b31f6ce0d77e7cf83e2fc
Blackout76 <olivier.leger@imerir.com>
2015-10-19 19:36:38 +0200

    Server Organise import and variables Server.py

--- Server/server.py
+++ Server/server.py
@@ -1,35 +1,31 @@
 ﻿#! /usr/bin/env python
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-from NetworkWebSocketClient import *
-from NetworkWebSocketDevice import *
-from NetworkHTTP import *
-from MapManager import *
 from threading import Thread
+import NetworkWebSocketClient
+import NetworkWebSocketDevice
+import NetworkHTTP
+import MapManager
 import threading
 import json
 import urlparse
 import sys
-
-ip = "0.0.0.0"
-portHTTP = 8080
-portWebSocketClient = 2639
-portWebSocketDevice = 2589
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # -------------------------------------
 #              MAIN               
 # -------------------------------------
 if __name__ == '__main__':
-	mapManager = MapManager("NomMap")
-	#mapManager.LoadFileMap()
-	#print mapManager.toDictFormat()
+	MapManager.mapManager = MapManager.MapManager("NomMap")
+	MapManager.mapManager.loadFileMap()
 	
-	thread_server_http = ServerHTTP("1")
-	thread_server_http.start()
-	thread_server_client = NetworkServerSocketClient("2",portWebSocketClient)
-	thread_server_client.start()
-	thread_server_device = NetworkServerSocketDevice("3",portWebSocketDevice)
-	thread_server_device.start()
-
+	NetworkHTTP.server_http = NetworkHTTP.ServerHTTP("1")
+	NetworkHTTP.server_http.start()
+	NetworkWebSocketClient.server_client = NetworkWebSocketClient.NetworkServerSocketClient("2")
+	NetworkWebSocketClient.server_client.start()
+	NetworkWebSocketDevice.server_device = NetworkWebSocketDevice.NetworkServerSocketDevice("3")
+	NetworkWebSocketDevice.server_device.start()
+	
 
 	
 	
