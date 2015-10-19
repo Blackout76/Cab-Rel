cb5daca7729ec00a0233eea223610178602e7d16
Blackout76 <olivier.leger@imerir.com>
2015-10-19 19:38:16 +0200

    Server Organise import and variables WebvSocketClient + Correct sended json format + Correct recepted json format + send Map to all new Client + Added Function broadcast all client

--- Server/NetworkWebSocketClient.py
+++ Server/NetworkWebSocketClient.py
@@ -1,31 +1,42 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 import json
 from threading import Thread
-from MapManager import *
 from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer
-import urlparse
-import sys
+import MapManager
 
 clients = []
+portWebSocketClient = 2639
+server_client = None
 
 class NetworkServerSocketClient(Thread):
-	def __init__(self, lettre, port):
+	def __init__(self, lettre):
 		Thread.__init__(self)
-		self.server = SimpleWebSocketServer('', port, ClientWebSocket)
+		self.server = SimpleWebSocketServer('', portWebSocketClient, ClientWebSocket)
 		print 'WEBSOCKET clients listenning ...'
 
 	def run(self):
 		self.server.serveforever()
+	
+	def broadcastAll(self,json):
+		for c in clients:
+			c.sendMessage(json.dumps(json,ensure_ascii=False))
 
 class ClientWebSocket(WebSocket):
 
 	def handleMessage(self):
 		# echo message back to client
-		self.sendMessage(self.data)
+		print json.loads(self.data)
+		# TODO
+		# Recept different type
+		
 
 	def handleConnected(self):
 		clients.append(self)
+		#Send map to client
+		self.sendMessage(json.dumps(MapManager.mapManager.toDictFormat(),ensure_ascii=False))
 		print self.address, 'connected'
-		self.sendMessage(u'Helo')
 
 	def handleClose(self):
 		clients.remove(self)
