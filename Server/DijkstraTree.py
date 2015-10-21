4e07fadebd0697c33071b4f1ef869ccf092068fc
Laurent Hilaire <goshirusama@gmail.com>
2015-10-21 08:35:55 +0200

    server python: starting dijktra algorithme

--- Server/DijkstraTree.py
+++ Server/DijkstraTree.py
@@ -2,6 +2,9 @@ class DijkstraTree:
 	def __init__(self, areasDict):
 		self.nodesList = []
 		self.arcsList = []
+		self.distNode = []
+		self.prevNode = []
+		self.nodeLeft = []
 		for area in areasDict:
 			for vertex in areasDict[area].verticesDict:
 				node = {}
@@ -32,6 +35,38 @@ class DijkstraTree:
 				arc["node2"] = nodeTo
 				arc["weight"] = bridge.bridgeWeight
 				self.arcsList.append(arc)
+
+	def initDijkstra(self, startPoint):
+		for node in self.nodesList:
+			distNode = {}
+			distNode["vertex"] = node["vertex"]
+			distNode["area"] = node["area"]
+			if node["vertex"] == startPoint["vertex"] and node["area"] == startPoint["area"]:
+				distNode["dist"] = 0
+			else:
+				distNode["dist"] = -1
+			self.distNode.append(distNode)
+			self.nodeLeft.append(node)
+			prevNode = {}
+			prevNode["prevNode"] = startPoint
+			prevNode["node"] = node
+			self.prevNode.append(prevNode)
+
+
+	def findShortestPath(self, startPoint, endPoint):
+		endIsFound = False
+		currentWeight = -1
+		currentNode = {}
+		self.initDijkstra(startPoint)
+		while len(self.nodeLeft) > 0 and endIsFound == False:
+			for node in self.distNode:
+				if (node["dist"] < currentWeight and node["dist"] >= 0) or currentWeight < 0:
+					currentWeight = node["dist"]
+					currentNode["vertex"] = node["vertex"]
+					currentNode["area"] = node["area"]
+
+		print "TODO"
+
 
 """			
 	def __init__(self,vertice):
