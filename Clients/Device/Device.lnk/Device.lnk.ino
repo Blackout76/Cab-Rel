31618b26e783da9ba20485828f22a5d428bba0f0
nicolasbzh <nicolas.casol@gmail.com>
2015-10-21 14:17:22 +0200

    Device Arduino fix some bug.

--- Clients/Device/Device.lnk/Device.lnk.ino
+++ Clients/Device/Device.lnk/Device.lnk.ino
@@ -33,6 +33,8 @@ int httpPort = 800;*/
 IPAddress httpAdress(169, 254, 83, 93);
 int httpPort = 8080;
 
+String addressServ;
+String portServ;
 String adrServ;
 int prtServ;
 char* idCab = "32";
@@ -60,6 +62,8 @@ int adc_key_in = 0;
 // Cab's State
 bool stateFreeCab = true;
 bool doConnect = false;
+bool acceptRequest = false;
+bool refusedRequest = false;
 
 // function definitions
 void parseJson(char *jsonString) ;
@@ -98,24 +102,90 @@ void setup()
 
 void loop ()
 {
-  if(doConnect)
-  wsClient.monitor();
+  if (stateFreeCab)
+  {
+    //Manage the button
+    lcd_key = read_LCD_buttons(); // read the buttons
+    switch (lcd_key) // depending on which button was pushed, we perform an action
+    {
+      case btnRIGHT:
+        {
+          Serial.print("accept request");
+          acceptRequest = true;
+          break;
+        }
+      case btnLEFT:
+        {
+          Serial.print("accept request");
+          refusedRequest = true;
+          break;
+        }
+      case btnUP:
+        {
+          doConnect = false;
+          closeWebSocketClient();
+          if (wsClient.connected()) Serial.println("connected again");
+          else  Serial.println(" disconnect");
+          break;
+        }
+      case btnDOWN:
+        {
+          break;
+        }
+      case btnSELECT:
+        {
+          doConnect = true;
+          connectServer(httpAdress, httpPort );
+          delay(1500);
+          break;
+        }
+      case btnNONE:
+        {
+
+        }
+        /* default:
+           {
+             Serial.print("pas d'appuie");
+             break;
+           }*/
+    }
+  }
+  if (doConnect)
+    wsClient.monitor();
 
   if (wsClient.connected())
   {
-
     if (stateFreeCab)
     {
-      wsClient.send("{\"cmd\": \"cabInfo\"}");
-      wsClient.onMessage(onMessage);
- 
-      displayInfo("FREE  d:0,0km","area :"+areaNow + ", street : " + streetNow);
+      if (acceptRequest == true)
+      {
+        wsClient.send("{\"cmd\":\"requestAnswer\" ,\"idRequest\": \"D32\",\"answer\":\"yes\"}");
+        //
+        acceptRequest = refusedRequest = false;
+        stateFreeCab = false;
+      }
+      else
+      {
+        if (refusedRequest == true)
+        {
+          wsClient.send("{\"cmd\":\"requestAnswer\" ,\"idRequest\": \"D32\",\"answer\":\"no\"}");
+          //
+          acceptRequest = refusedRequest = false;
+        }
+        else
+        {
+          Serial.println("j'envoi cabInfo");
+          wsClient.send("{\"cmd\": \"cabInfo\"}");
+          wsClient.onMessage(onMessage);
+
+          displayInfo("FREE  d:0,0km", "area :" + areaNow + ", street : " + streetNow);
+        }
+      }
     }
     else
     {
-      lcd.print("BUSY");
+      Serial.println("je suis dans le busy");
     }
-
     wsClient.onError(onError);
   }
   else
@@ -125,63 +195,23 @@ void loop ()
     lcd.setCursor(0, 1);
     lcd.print("    Connect");
     delay(500);
-  }
 
-
-  //Manage the button
-  lcd_key = read_LCD_buttons(); // read the buttons
-  switch (lcd_key) // depending on which button was pushed, we perform an action
-  {
-    case btnRIGHT:
-      {
-
-        wsClient.send("{\"cmd\":\"requestAnswer\",\"idRequest\":\"D32\",\"answer\":\"no\"}");
-        break;
-      }
-    case btnLEFT:
-      {
-        wsClient.send("{\"cmd\":\"requestAnswer\" ,\"idRequest\": \"D32\",\"answer\":\"yes\"}");
-        break;
-      }
-    case btnUP:
-      {
-        doConnect = false;
-        closeWebSocketClient();
-        if (wsClient.connected()) Serial.println("connected again");
-        else  Serial.println(" disconnect");
-        break;
-      }
-    case btnDOWN:
-      {
-        break;
-      }
-    case btnSELECT:
-      {
-        doConnect = true;
-        connectServer(httpAdress, httpPort );
-        delay(1500);
-        break;
-      }
-    case btnNONE:
-      {
-
-      }
   }
 }
 
-void displayInfo(String l1,String l2)
+void displayInfo(String l1, String l2)
 {
- 
+
   lcd.setCursor(0, 0);
   lcd.print(l1);
   lcd.setCursor(scrollCursor, 1);
-  lcd.print(l2.substring(stringStart,stringStop));
+  lcd.print(l2.substring(stringStart, stringStop));
   delay(300);
   lcd.clear();
-  if(stringStart == 0 && scrollCursor > 0){
+  if (stringStart == 0 && scrollCursor > 0) {
     scrollCursor--;
     stringStop++;
-  } else if (stringStart == stringStop){
+  } else if (stringStart == stringStop) {
     stringStart = stringStop = 0;
     scrollCursor = screenWidth;
   } else if (stringStop == l2.length() && scrollCursor == 0) {
@@ -194,7 +224,8 @@ void displayInfo(String l1,String l2)
 
 void connectServer(IPAddress Adress , int HttpPort )
 {
-
+  addressServ = "";
+  portServ = "";
   String getHttp = "";
   adrServ = "";
   char tmp;
@@ -211,11 +242,12 @@ void connectServer(IPAddress Adress , int HttpPort )
     bool cond = false;
     while (clientHttp.available())
     {
+      /*
       char c = clientHttp.read();
       getHttp += c;
+      */
 
-      /*
-          char c = clientHttp.read();
+      char c = clientHttp.read();
       Serial.print(c);
       if (adr)
       {
@@ -225,48 +257,55 @@ void connectServer(IPAddress Adress , int HttpPort )
         }
         else
         {
-          if (cond == true) prtServ += c;
-          else  adrServ += c;
+          if (cond == true) portServ += c;
+          else  addressServ += c;
         }
       }
       if (c == '/' and tmp == '/') adr = true;
       tmp = c;
 
-       */
+
     }
 
     //disconnect to Http
     closeConnectionHttp();
+    /*
+        Serial.println("gethttp" + getHttp);
+        int cc = getHttp.lastIndexOf("{");
+        aJsonObject* root = aJson.parse(strToChar(getHttp.substring(cc)));
 
-    Serial.println("gethttp" + getHttp);
-    int cc = getHttp.lastIndexOf("{");
-    aJsonObject* root = aJson.parse(strToChar(getHttp.substring(cc)));
+        if (root != NULL) {
+          //Serial.println("Parsed successfully 1 " );
+          aJsonObject* prt = aJson.getObjectItem(root, "prt");
+          aJsonObject* addr = aJson.getObjectItem(root, "addr");
+          if (prt != NULL)
+          {
+            prtServ = prt->valueint;
 
-    if (root != NULL) {
-      //Serial.println("Parsed successfully 1 " );
-      aJsonObject* prt = aJson.getObjectItem(root, "prt");
-      aJsonObject* addr = aJson.getObjectItem(root, "addr");
-      if (prt != NULL)
-      {
-        prtServ = prt->valueint;
-
-      }
-      if (addr != NULL)
-      {
-        adrServ = addr->valuestring;
-      }
-    }
+          }
+          if (addr != NULL)
+          {
+            adrServ = addr->valuestring;
+          }
+        }
 
 
-    Serial.println("                  adr :" + adrServ);
-    Serial.print("                   prt: ");
-    Serial.println(prtServ);
+        Serial.println("                  adr :" + adrServ);
+        Serial.print("                   prt: ");
+        Serial.println(prtServ);
+
+        //adding the chain end character
+        adrServ.concat("\0");
+
+        connectWebSocketClient(adrServ, prtServ);*/
+
+    Serial.println("adr :" + addressServ);
+    Serial.println("prt: " + portServ);
 
     //adding the chain end character
-    adrServ.concat("\0");
+    addressServ.concat("\0");
 
-    //connectWebSocketClient(adrServ, prtServ);
-    connectWebSocketClient(adrServ, prtServ);
+    connectWebSocketClient(addressServ, portServ.toInt());
   }
   else
   {
