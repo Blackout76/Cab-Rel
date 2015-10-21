83e58b0833a2363a010d57c908cfa10398b368db
ameliep <amelie.pereira@imerir.com>
2015-10-21 19:02:41 +0200

    some changes on loadMap function

--- Clients/Tablet/GNE/app/src/main/java/com/example/blackout/gne/Map/MapManager.java
+++ Clients/Tablet/GNE/app/src/main/java/com/example/blackout/gne/Map/MapManager.java
@@ -2,34 +2,37 @@ package com.example.blackout.gne.Map;
 
 import android.util.Log;
 
-import com.example.blackout.gne.Network.AsyncWebSocket;
+import com.example.blackout.gne.MainActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Observable;
-import java.util.Observer;
 
 
-public class MapManager extends Observable implements AsyncWebSocket {
+public class MapManager extends Observable {
 	private HashMap<Object, MapArea> areas;
+	public static String areaToLoad = "";
 
 	@SuppressWarnings("unchecked")
 	public void loadMap(JSONObject mapJson) {
 		this.areas = new HashMap<>();
 		try {
 			//JSONArray li=  (JSONArray)mapJson.get("areas");
-
 			JSONArray li = mapJson.getJSONArray("areas");
-			for (int i = 0; i < li.length(); i++)
+
+			for (int i = 0; i < li.length(); i++) {
 				this.areas.put(li.getJSONObject(i).get("name"), new MapArea(li.getJSONObject(i)));
+				areaToLoad=li.getJSONObject(i).getString("name");
+			}
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 		Log.i("MAP", "map generated !");
+		MainActivity.ihm.loadRender(areaToLoad);
+
 	}
 
 
@@ -37,16 +40,30 @@ public class MapManager extends Observable implements AsyncWebSocket {
 		return this.areas.get(name);
 	}
 
-
-	@Override
-	public void onWebSocketMessage(JSONObject json) {
-		try {
-			if (json.getJSONArray("areas") != null) {
-				this.loadMap(json);
-				//Cab.renderer.generateArea(getAreaByName("Quartier Sud"));
+	public void loadNextArea(){
+		String first = "";
+		String nameNext = "";
+		boolean found = false;
+		for(Object areaName : areas.keySet()){
+			if(found){
+				nameNext = areaName.toString();
+				break;
 			}
-		} catch (JSONException e) {
-			e.printStackTrace();
+			if(!found){
+				if(first.equals(""))
+					first = areaName.toString();
+				if(areaName.equals(MainActivity.ihm.getNameOfActiveArea()))
+					found = true;
+			}
+
+		}
+        Log.e("zerty", nameNext);
+		if(nameNext.equals(""))
+			nameNext = first;
+		if(nameNext != null && !nameNext.equals("")){
+			MainActivity.ihm.loadRender(nameNext);
+
+			MainActivity.ihm.postInvalidate();
 		}
 	}
 }
